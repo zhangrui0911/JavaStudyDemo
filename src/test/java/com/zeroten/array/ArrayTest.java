05efcef4ce8e187d65cd5a9d16fe4c6898799ae4
zhangrui0911 <449097385@qq.com>
2020-01-19 00:08:08 +0800

    数组遍历

--- src/test/java/com/zeroten/array/ArrayTest.java
+++ src/test/java/com/zeroten/array/ArrayTest.java
@@ -3,6 +3,7 @@ package com.zeroten.array;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Zr
@@ -56,5 +57,40 @@ public class ArrayTest {
         Integer[] arr9 = {2,9,7,null,0,1};
         //arr9 === [2, 9, 7, null, 0, 1]
         System.out.println("arr9 === " + Arrays.toString(arr9));
+    }
+
+    @Test
+    public void testBianLi() {
+
+        //使用三种方式遍历数组 Integer[] arr = {1,2,3,4,5};
+        Integer[] arr = {1,2,3,4,5};
+
+        //for循环
+        for (int i = 0; i < arr.length; i++) {
+//            System.out.println("下标：" + i + ", 存放的值是" + arr[i]);
+            System.out.println(String.format("下标： %d , 存放的值是 %d", i,arr[i]));
+        }
+
+        //for each
+        int index = 0;
+        for (Integer integer : arr) {
+            System.out.println("第 " + index + "次取到 " + integer);
+            index++;
+        }
+
+        //将数组转换为一个List集合
+        List<Integer> integerList = Arrays.asList(arr);
+        //调用List集合的forEach方法，参数前面的是获取的值，-> 后面的是要执行的操作语句，
+        // 如果 -> 后面只有一条语句，不用写{}
+        //integerList.forEach(val -> System.out.println("值是" + val));
+        //如果是多条语句，则需要使用{ }将要执行的语句括起来
+        integerList.forEach(val -> {
+            System.out.println("值是" + val);
+            if(val % 2 == 0){
+                System.out.println("  它是一个偶数");
+            } else {
+                System.out.println("  它是一个奇数");
+            }
+        });
     }
 }
